b3f246806952daf5867876ef0015eb6d3b2142f0
DmitriyBaev <100maverick100@gmail.com>
2021-05-05 20:30:49 +0300

    homeWork 1.2 ver.4

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -6,94 +6,38 @@ const sliders = document.getElementsByClassName("slider__item");
 const arrSliders = Array.from(sliders);
 const dots = document.getElementsByClassName("slider__dot");
 const arrDots = Array.from(dots);
-
-// function changedImgRight() {
-//   arrSliders[i].className = "slider__item";
-//   i < arrSliders.length - 1
-//     ? (arrSliders[i + 1].className += " slider__item_active")
-//     : ((i = -1), (arrSliders[i + 1].className += " slider__item_active"));
-
-//   i += 1;
-
-//   activeDot();
-// }
-
-// function changedImgLeft() {
-//   arrSliders[i].className = "slider__item";
-//   i > 0
-//     ? (arrSliders[i - 1].className += " slider__item_active")
-//     : ((i = arrSliders.length),
-//       (arrSliders[i - 1].className += " slider__item_active"));
-
-//   i -= 1;
-
-//   activeDot();
-// }
-
-// function activeDot() {
-//   const index = arrSliders.findIndex((element) =>
-//     element.className.includes("slider__item_active")
-//   );
-//   console.log(index);
-//   arrDots.forEach((elem) => {
-//     elem.className = "slider__dot";
-//   });
-//   arrDots[index].className += " slider__dot_active";
-// }
-
-// arrDots.forEach((elem) => {
-//   elem.onclick = () => {
-//     const index = arrDots.indexOf(elem);
-//     arrSliders[index].className += " slider__item_active";
-//   };
-// });
-
-// let i = 0;
-// rightButton.onclick = changedImgRight;
-// leftButton.onclick = changedImgLeft;
-
-let index = arrSliders.findIndex((element) =>
-  element.className.includes("slider__item_active")
-);
+let index, exIndex;
 
 function activeSlideDot(index) {
+  exIndex = arrSliders.findIndex((element) =>
+    element.className.includes("slider__item_active")
+  );
+  arrSliders[exIndex].className = "slider__item";
+  arrDots[exIndex].className = "slider__dot";
+
   arrSliders[index].className += " slider__item_active";
   arrDots[index].className += " slider__dot_active";
 }
 
 function changedImgRight() {
-  arrSliders[index].className = "slider__item";
-  arrDots[index].className = "slider__dot";
+  index = arrSliders.findIndex((element) =>
+    element.className.includes("slider__item_active")
+  );
   index < arrSliders.length - 1 ? (index += 1) : (index = 0);
 
   activeSlideDot(index);
 }
 
 function changedImgLeft() {
-  arrSliders[index].className = "slider__item";
-  arrDots[index].className = "slider__dot";
+  index = arrSliders.findIndex((element) =>
+    element.className.includes("slider__item_active")
+  );
   index > 0 ? (index -= 1) : (index = arrSliders.length - 1);
-
+  
   activeSlideDot(index);
 }
 
-if (
-  arrDots.findIndex((elem) => {
-    elem.onclick;
-    return elem;
-  })
-) {
-  index = arrDots.indexOf(elem);
-  activeSlideDot(index);
-}
-
-// arrDots.forEach((elem) => {
-//     elem.onclick = () => {
-
-//       index = arrDots.indexOf(elem);
-//       activeSlideDot(index)
-//     };
-//   });
+arrDots.forEach((elem, index) => (elem.onclick = () => activeSlideDot(index)));
 
 rightButton.onclick = changedImgRight;
 leftButton.onclick = changedImgLeft;
